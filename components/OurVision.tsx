8be33841c85907eeb017e5d8b060a9edce7cce3f
guidolarosa <guido.glarosa@gmail.com>
2022-07-14 17:19:52 -0300

    Updated keys

--- components/OurVision.tsx
+++ components/OurVision.tsx
@@ -14,7 +14,7 @@ const OurVision = (props: any) => {
         <Box className="content-block">
           <h2>Websites should bring joy</h2>
           <p>
-            Marketing websites should be fun, exciting. Remember when the internet was young and you looked. If using your website doesn't bring joy to the people who use it, then everything else falls apart. 
+            Marketing websites should be fun, exciting. Remember when the internet was young and you looked. If using your website doesn&lsquo;t bring joy to the people who use it, then everything else falls apart. 
             Surfing the web should be a delight, and our job is making it delightful for our users. 
           </p>
         </Box>
@@ -27,7 +27,7 @@ const OurVision = (props: any) => {
         <Box className="content-block">
           <h2>Websites should be fast</h2>
           <p>
-            As it's often said: performance is a feature. We take this very
+            As it&lsquo;s often said: performance is a feature. We take this very
             seriously and we expect from every site we build to be optimized and
             performant.
           </p>
@@ -35,7 +35,7 @@ const OurVision = (props: any) => {
         <Box className="content-block">
           <h2>Websites should be beautiful</h2>
           <p>
-            Sometimes beautiful means simple, but sometimes it doesn't. We are
+            Sometimes beautiful means simple, but sometimes it doesn&lsquo;t. We are
             here to bring the best designs to life.
           </p>
         </Box>
@@ -44,7 +44,7 @@ const OurVision = (props: any) => {
           <p>
             This might be the most important point. Every site that we build
             should serve a purpouse and meet the expectations of the client. We
-            don't want to add more unnecesary noise to our global network.
+            don&lsquo;t want to add more unnecesary noise to our global network.
           </p>
         </Box>
       </Box>
