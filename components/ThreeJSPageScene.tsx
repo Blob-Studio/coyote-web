f3ffa18e1dbb4c54f1b35454e4a16a182ec445fe
Cherna <chernatom@gmail.com>
2022-09-27 01:16:45 -0300

    Changed 3d into a distorted sphere animation

--- components/ThreeJSPageScene.tsx
+++ components/ThreeJSPageScene.tsx
@@ -2,10 +2,10 @@
 import styled from "styled-components";
 
 import ReactDOM from "react-dom";
-import React, { Suspense, useEffect, useState, useMemo, useRef } from "react";
-import { Canvas, useLoader, useFrame, useThree } from "@react-three/fiber";
-import { Vector3, TextureLoader, Vector2, PointLightHelper, DoubleSide } from "three";
-import { OrbitControls, useHelper } from "@react-three/drei";
+import React, { Suspense, useEffect, useMemo, useRef, useState } from "react";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import { Vector2, PointLightHelper } from "three";
+import { OrbitControls, useHelper, MeshDistortMaterial, Environment } from "@react-three/drei";
 import {
   EffectComposer,
   RenderPass,
@@ -19,71 +19,6 @@ import {
   ScanlineEffect,
   GlitchEffect,
 } from "postprocessing";
-
-function Image() {
-  const texture = useLoader(TextureLoader, "/img/grids/grid_1.png");
-  return (
-    <mesh rotation-x={Math.PI * -0.5}>
-      <planeBufferGeometry attach="geometry" args={[3, 3]} />
-      <meshBasicMaterial attach="material" map={texture} />
-    </mesh>
-  );
-}
-
-function Banner() {
-  const texture = useLoader(TextureLoader, "/img/decorations/banner.png");
-  return (
-    <mesh rotation-y={Math.PI * 0.5} position={new Vector3(1.25, 0.6, 1)}>
-      <planeBufferGeometry attach="geometry" args={[1, 0.5]} />
-      <meshBasicMaterial attach="material" map={texture} />
-    </mesh>
-  );
-}
-
-function Badge() {
-  const texture = useLoader(TextureLoader, "/img/grids/badge.png");
-  return (
-    <mesh rotation-y={Math.PI * 0.5} position={new Vector3(0.75, 1, -1)}>
-      <planeBufferGeometry attach="geometry" args={[1, 1]} />
-      <meshBasicMaterial
-        attach="material"
-        map={texture}
-        opacity={1}
-        transparent
-      />
-    </mesh>
-  );
-}
-
-function Window() {
-  const texture = useLoader(TextureLoader, "/img/grids/window.png");
-  return (
-    <mesh rotation-y={Math.PI * 0.5} position={new Vector3(0.25, 0.75, 0)}>
-      <planeBufferGeometry attach="geometry" args={[2, 1]} />
-      <meshBasicMaterial
-        attach="material"
-        map={texture}
-        opacity={1}
-        transparent
-      />
-    </mesh>
-  );
-}
-
-function Atom() {
-  const texture = useLoader(TextureLoader, "/img/grids/atom.png");
-  return (
-    <mesh rotation-y={Math.PI * 0.5} position={new Vector3(-0.5, 1.25, 1)}>
-      <planeBufferGeometry attach="geometry" args={[1, 1]} />
-      <meshBasicMaterial
-        attach="material"
-        map={texture}
-        opacity={1}
-        transparent={true}
-      />
-    </mesh>
-  );
-}
 
 const Effect = ({ children }: any) => {
   const { gl, camera, scene } = useThree();
@@ -122,7 +57,7 @@ const Effect = ({ children }: any) => {
 
 const CustomPointLight = ({ color, intensity, position }: any) => {
   const pLight = useRef();
-  useHelper(pLight, PointLightHelper, intensity || 1, color);
+  // useHelper(pLight, PointLightHelper, intensity || 1, color);
   return (
     <pointLight
       castShadow
@@ -135,7 +70,77 @@ const CustomPointLight = ({ color, intensity, position }: any) => {
   );
 }
 
+const DistortedSphereMesh = () => {
+  const BASE_DISTORT = 0.65;
+  const MAX_DISTORT = 1.2;
+  const BASE_SPEED = 1.15;
+  const MAX_SPEED = 4;
+  const [mouseOver, setMouseOver] = useState<boolean>(false);
+  const mesh = useRef();
+
+  useEffect(() => {
+    console.log(mesh);
+  }, [mesh])
+
+  /*
+    Ideas:
+    - Interpolate values over time so the shape doesn't change abruptly
+  */
+  useFrame(() => {
+    let curVal = mesh.current._distort.value;
+    // console.log(curVal, MAX_DISTORT);
+    if (mouseOver && curVal <= MAX_DISTORT) {
+      mesh.current._distort.value = curVal + 0.05;
+      console.log(curVal);
+    }
+    else if (!mouseOver && curVal > BASE_DISTORT) {
+      mesh.current._distort.value = curVal - 0.05;
+      console.log(curVal);
+    }
+  });
+
+  return (
+    <mesh
+      castShadow
+      receiveShadow
+      onPointerEnter={e => {
+        // setDistort(0.65);
+        // setDistortSpeed(2);
+        setMouseOver(true);
+      }}
+      onPointerLeave={e => {
+        // resetDistort();
+        setMouseOver(false);
+      }}
+    >
+      <sphereBufferGeometry attach="geometry" args={[1.25, 100, 100]}/>
+      {/* <meshStandardMaterial
+        attach="material"
+        color="#383838"
+        metalness={0.8}
+        roughness={0.25}
+      /> */}
+      <MeshDistortMaterial
+        ref={mesh}
+        color="#666666"
+        distort={BASE_DISTORT}
+        speed={BASE_SPEED}
+        metalness={0.5}
+        roughness={0.5}
+      />
+    </mesh>
+  );
+} 
+
 const ThreeJSPageScene = (props: any) => {
+  // const [distort, setDistort] = useState<number>(0.65);
+  // const [distortSpeed, setDistortSpeed] = useState<number>(1.15);
+
+  // const resetDistort = () => {
+  //   setDistort(0.65);
+  //   setDistortSpeed(1.15);
+  // };
+
   return (
     <Canvas
       gl={{
@@ -146,26 +151,25 @@ const ThreeJSPageScene = (props: any) => {
       }}
       shadows
       // shadowMap={[2000, 2000]}
-      camera={{ position: [-10, -5, 10] }}
+      camera={{ position: [-2, -5, 2] }}
     >
       <Suspense fallback={null}>
-        <OrbitControls />
-        <CustomPointLight color={"#9cb1b1"} position={[2, 5, 5]}/>
-        <CustomPointLight color={"#b8cfd4"} position={[-2, -5, 5]}/>
-        <mesh receiveShadow position={[0, 0, -2]}>
+        {/* <OrbitControls /> */}
+        <ambientLight intensity={1.5} />
+        <CustomPointLight color={"#68a1c7"} position={[2, 5, 5]}/>
+        <CustomPointLight color={"#c6d1d3"} position={[-2, -2, -2]}/>
+        <Environment
+          background={false}
+          // files={undefined}
+          // path="/"
+          preset='dawn'
+        />
+        {/* <mesh receiveShadow position={[0, 0, -2]}>
           <planeBufferGeometry attach="geometry" args={[10, 10]} />
           <meshStandardMaterial attach="material" color="#000000" />
-        </mesh>
-        <mesh castShadow>
-          <sphereBufferGeometry attach="geometry" args={[1, 40, 40]}/>
-          <meshStandardMaterial attach="material" color="#383838" />
-        </mesh>
-        {/* <Image />
-        <Badge />
-        <Window />
-        <Atom />
-        <Banner /> */}
-        {/* <Effect /> */}
+        </mesh> */}
+        <DistortedSphereMesh />
+        <Effect />
       </Suspense>
       {/* <fog attach="fog" args={["#4b4d50", 0, 12]} /> */}
       {/* <gridHelper /> */}
